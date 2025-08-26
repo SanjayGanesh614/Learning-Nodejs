0503de4819256addc4dd50ec1a33aade696fc575
Sanjay Ganesh K Barade <sgbarade@gmail.com>
2025-08-26 22:51:10 +0530

    Day 1

---
+++ hello.js
@@ -0,0 +1,6 @@
+const math = require("./math");
+console.log("Hey There I  am Learning JS!");
+
+console.log("This is my first Nodejs code");
+
+console.log("the is  the math value", math.addfn(5,7) );
